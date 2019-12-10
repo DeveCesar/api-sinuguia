e54de7e0c33551a6915d62f24161eaad239d3be6
Cesar Miguel Mendoza Gonzalez <cesarmiguel0516@hotmail.com>
2019-12-10 13:45:34 -0500

    v.0.1

--- database/config.js
+++ database/config.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 
 const conexionDB = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sinuguia'
+    host: 'b7uvrfsuznphr5lcbbg3-mysql.services.clever-cloud.com',
+    user: 'ukwsvoujvegsot10',
+    password: 'Nkcr7arU83Z0MXn1P9E0',
+    database: 'b7uvrfsuznphr5lcbbg3',
+    port: 3306
 });
 
  conexionDB.connect((err) => {
     if (err) throw err;
-    console.log('Mysql Connected...');
+    console.log('DB_CONNECT...');
 });
 
 
